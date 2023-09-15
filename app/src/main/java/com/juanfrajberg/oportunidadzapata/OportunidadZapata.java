4d4245f30f52e2c7efd486b2a73d4d0cf88c02ee
Juan Frajberg <juan.frajberg@gmail.com>
2023-09-14 22:23:10 -0300

    Improved code of notification when a new proposal is added

--- app/src/main/java/com/juanfrajberg/oportunidadzapata/OportunidadZapata.java
+++ app/src/main/java/com/juanfrajberg/oportunidadzapata/OportunidadZapata.java
@@ -3,6 +3,14 @@ package com.juanfrajberg.oportunidadzapata;
 import android.app.Application;
 import android.content.Intent;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class OportunidadZapata extends Application {
     //Variable que almacena el número de propuestas que hay (terminar de configurar bien)
     public int proposalNumbers;
@@ -12,8 +20,32 @@ public class OportunidadZapata extends Application {
 
     @Override
     public void onCreate() {
+        //Código básico para que se inicie la clase
         super.onCreate();
 
+        //Se lee el número de las propuestas actuales y al cambiar se envía una notificación
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference reference = database.getReference("1JcKn4lV9YC5cF8o_QyekJ7-72u-bRn748CLrLc9jTD0/application/1");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                //Por si se borran los datos o no se puede acceder a ellos
+                try {
+                    int numberProposals = snapshot.child("numberProposals").getValue(Integer.class);
+                    sendNewProposalNotification((numberProposals));
+                } catch (Exception e) {}
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    //Función para enviar una notificación sobre propuestas nuevas
+    public void sendNewProposalNotification(long numberProposals) {
         //Código que envía una notificación cuando se añade una propuesta
         Intent intentToOpenActivity = new Intent(getApplicationContext(), ContactActivity.class);
         NotificationBroadcast.showNotification(getApplicationContext(), "Oportunidad Zapata", getApplicationContext().getResources().getString(R.string.newproposalnotificacion_text), intentToOpenActivity, 1);
