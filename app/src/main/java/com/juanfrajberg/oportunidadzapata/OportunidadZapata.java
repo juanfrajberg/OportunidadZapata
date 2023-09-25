add2c280e3e61964a51aa56bf0faa9279fc6032f
Juan Frajberg <juan.frajberg@gmail.com>
2023-09-25 17:18:53 -0300

    Improved notification when new proposal is added and Toast message about the version

--- app/src/main/java/com/juanfrajberg/oportunidadzapata/OportunidadZapata.java
+++ app/src/main/java/com/juanfrajberg/oportunidadzapata/OportunidadZapata.java
@@ -2,6 +2,7 @@ package com.juanfrajberg.oportunidadzapata;
 
 import android.app.Application;
 import android.content.Intent;
+import android.content.SharedPreferences;
 
 import androidx.annotation.NonNull;
 
@@ -13,7 +14,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class OportunidadZapata extends Application {
     //Variable que almacena el número de propuestas que hay (terminar de configurar bien)
-    public int proposalNumbers;
+    public static String proposalNumbers;
 
     //Para llamar a este valor desde cualquier actividad
     //int proposalNumbers = ((OportunidadZapata) getApplicationContext()).proposalNumbers;
@@ -22,6 +23,13 @@ public class OportunidadZapata extends Application {
     public void onCreate() {
         //Código básico para que se inicie la clase
         super.onCreate();
+
+        //Ver si se han añadido propuestas nuevas o no
+        //if (StartActivity.firstRun) {}
+
+        SharedPreferences sharedPref = getSharedPreferences("OZ", MODE_PRIVATE);
+        proposalNumbers = String.valueOf(sharedPref.getInt("proposalsNumber", 0));
+        //Toast.makeText(getApplicationContext(), proposalNumbers, Toast.LENGTH_SHORT).show();
 
         //Se lee el número de las propuestas actuales y al cambiar se envía una notificación
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -33,7 +41,22 @@ public class OportunidadZapata extends Application {
                 //Por si se borran los datos o no se puede acceder a ellos
                 try {
                     int numberProposals = snapshot.child("numberProposals").getValue(Integer.class);
-                    sendNewProposalNotification((numberProposals));
+
+                    SharedPreferences sharedPreferences = getSharedPreferences("OZ", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putInt("proposalsNumber", numberProposals);
+                    editor.apply();
+
+                    if (numberProposals != Integer.parseInt(proposalNumbers)) {
+                        if (numberProposals > (Integer.parseInt(proposalNumbers) + 1)) {
+                            sendNewProposalNotification(2);
+                        }
+                        else {
+                            if (!(numberProposals < Integer.parseInt(proposalNumbers))) {
+                                sendNewProposalNotification(1);
+                            }
+                        }
+                    }
                 } catch (Exception e) {}
             }
 
@@ -45,9 +68,13 @@ public class OportunidadZapata extends Application {
     }
 
     //Función para enviar una notificación sobre propuestas nuevas
-    public void sendNewProposalNotification(long numberProposals) {
+    public void sendNewProposalNotification(int notification) {
         //Código que envía una notificación cuando se añade una propuesta
         Intent intentToOpenActivity = new Intent(getApplicationContext(), ContactActivity.class);
-        NotificationBroadcast.showNotification(getApplicationContext(), "Oportunidad Zapata", getApplicationContext().getResources().getString(R.string.newproposalnotificacion_text), intentToOpenActivity, 1);
+        if (notification == 1) {
+            NotificationBroadcast.showNotification(getApplicationContext(), "Oportunidad Zapata", getApplicationContext().getResources().getString(R.string.newproposalnotificacion_single_text), intentToOpenActivity, 1);
+        } else if (notification == 2) {
+            NotificationBroadcast.showNotification(getApplicationContext(), "Oportunidad Zapata", getApplicationContext().getResources().getString(R.string.newproposalnotificacion_many_text), intentToOpenActivity, 1);
+        }
     }
 }
